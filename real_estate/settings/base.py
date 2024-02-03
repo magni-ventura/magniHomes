ab0ec6d7e5e77f5911b8548a4b10b7917800a00c
magniHomes <magniventures@proton.me>
2024-02-03 16:51:54 +0100

    feat: add custom user model

--- real_estate/settings/base.py
+++ real_estate/settings/base.py
@@ -140,3 +140,64 @@ MEDIA_ROOT = BASE_DIR / "mediafiles"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "users.User"
+
+#Defining The Logger
+import logging
+import logging.config
+from django.utils.log import DEFAULT_LOGGING
+
+#This will choose a Python log instance or create one
+logger = logging.getLogger(__name__)
+
+#Global log levels
+LOG_LEVEL = "INFO"
+#Configure log levels
+logging.config.dictConfig({
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+
+        "console": {
+            "format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+        },
+
+        "file": {"format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+        },
+        #Django server formaters
+        "django.server": DEFAULT_LOGGING["formatters"]["django.server"],
+    },
+
+    #Defining the handlers
+    "handlers": {
+        #console handler
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "console",           
+        },
+        #file handler
+        "file": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "formatter": "file",
+            "filename": "logs/real_estate.log"
+        },
+        #Django server formaters
+        "django.server": DEFAULT_LOGGING["handlers"]["django.server"],
+    },
+
+    #Defining the loggers
+    "loggers": {
+        "": {
+            "level": "INFO", "handlers":["console", "file"], "propagate":False
+        },
+        #Defining apps for the loggers propagate is required to prevent a double login with the root logger
+        "apps": {
+            "level": "INFO", "handlers":["console"], "propagate":False
+        },
+        #Django server formaters
+        "django.server": DEFAULT_LOGGING["loggers"]["django.server"],
+    }
+
+})
--- real_estate/urls.py
+++ real_estate/urls.py
@@ -17,8 +17,12 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('supersecret/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = "Real Estate Admin"
+admin.site.site_title = "Real Estate Admin Portal"
+admin.site.index_title = "Welcome to The Real Estate Portal"
